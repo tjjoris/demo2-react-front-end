6a739cab72b1aae2e689a3d4142294eb7bbe1804
tjjoris <tjjoris@gmail.com>
2025-10-28 13:09:39 -0400

    removed placeholder in App.js and added menu, container, and container item divs using semtnic

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,12 @@ import React, { Component } from "react";
 class App extends Component {
     // Render method to display the component
     render() {
-        return <div>
-            App
+        return <div className="ui fixed inverted menu">
+            <div className="ui container">
+                <a href="/#" className="header item">
+                    React JS CRUD with Laravel API
+                </a>
+            </div>
         </div>;
     }
 }
