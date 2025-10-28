2ecaeab213eb8cbd2f2652ea618a955fc693cf07
tjjoris <tjjoris@gmail.com>
2025-10-28 14:09:24 -0400

    edited customerList.js to replace placeholder with palceholer table fields.

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -3,7 +3,30 @@ import React, { Component } from "react";
 class CustomerList extends Component {
     render() {
         return (
-            <div> CustomerList </div>
+            <div className="data">
+                <table className="ui celled table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>E-mail</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>luke</td>
+                            <td>myemail</td>
+                            <td>
+                                <button className="mini ui blue button">Edit</button>
+                                <button className="mini ui red button">Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div >
         );
     }
 }
