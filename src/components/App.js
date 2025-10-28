5cc3b5a4ac435a2d4ed31951f29b0b4d5d058e57
tjjoris <tjjoris@gmail.com>
2025-10-28 13:24:37 -0400

    added MyForm with placeholder, but it is only visible if text appears in the div containing MyForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
+import MyForm from "./MyForm";
 
 // Main App component
 class App extends Component {
     // Render method to display the component
     render() {
-        return <div className="ui fixed inverted menu">
-            <div className="ui container">
-                <a href="/#" className="header item">
-                    React JS CRUD with Laravel API
-                </a>
+        return (
+            <div>
+                <div className="ui fixed inverted menu">
+                    <div className="ui container">
+                        <a href="/#" className="header item">
+                            React JS CRUD with Laravel API
+                        </a>
+                    </div>
+                </div>
+                <MyForm />
+                <div className="ui main container">
+                    Test
+                    <MyForm />
+                </div>
             </div>
-        </div>;
+        );
     }
 }
 
---
+++ src/components/MyForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class MyForm extends Component {
+    render() {
+        return <div>MyForm</div>;
+    }
+}
+
+export default MyForm;
