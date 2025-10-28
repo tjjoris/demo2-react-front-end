a833e4697f2a8af974a887d9da3917710e3f8c45
tjjoris <tjjoris@gmail.com>
2025-10-28 12:46:57 -0400

    added index.js and App.js, and index.js renders the main App component in the root element

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Render the main App component into the root element
+ReactDOM.render(<App />, document.querySelector("#root"))
