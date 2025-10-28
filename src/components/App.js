c3d89d39710bec59d3faacd7ce8f6368b3f6355c
tjjoris <tjjoris@gmail.com>
2025-10-28 12:50:01 -0400

    added App.js placeholder content

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+// Main App component
+class App extends Component {
+    // Render method to display the component
+    render() {
+        return <div>
+            App
+        </div>
+    }
+}
+
+export default App;
