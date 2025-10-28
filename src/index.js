1cbecfd9a605bfeb1274c36eef4ae624d3b87e27
tjjoris <tjjoris@gmail.com>
2025-10-28 12:59:50 -0400

    updated index.js to use the render API for version 18+

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
+//this was the old way before React 18
 import ReactDOM from "react-dom";
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
 
-// Render the main App component into the root element
-ReactDOM.render(<App />, document.querySelector("#root"))
+//this was the old way before React 18
+// ReactDOM.render(<App />, document.querySelector("#root"));
+
+//render the App component using React 18+ API
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
