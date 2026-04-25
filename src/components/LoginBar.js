752aeb529b5e37b6e164196f4e865190d956bb58
tjjoris <tjjoris@gmail.com>
2026-04-25 15:00:22 -0400

    created LoginBar component.

--- src/components/LoginBar.js
+++ src/components/LoginBar.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 
 class LoginBar extends Component {
-
+    async registerBob() {
+        const response = await fetch("http://127.0.0.1:8000/api/register", {
+            method: "post",
+            body: JSON.stringify({
+                name: "Bob",
+                email: "bob@example.com",
+                password: "pw1234",
+                password_confirmation: "pw1234"
+            })
+        });
+        const data = await response.json();
+        console.log(data);
+    };
     render() {
         return (
             <>
@@ -15,6 +27,7 @@ class LoginBar extends Component {
                     </div>
 
                 </form>
+                <button onClick={() => this.registerBob()}>Register Bob</button>
             </>
         )
     }
