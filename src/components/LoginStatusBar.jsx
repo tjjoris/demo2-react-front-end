8584ecf052d404b6e7b5a91830a11c3c0c5878e8
tjjoris <tjjoris@gmail.com>
2026-05-10 14:24:22 -0400

    making register form and button which has no function yet.

--- src/components/LoginStatusBar.jsx
+++ src/components/LoginStatusBar.jsx
@@ -4,13 +4,34 @@ import RegisterBar from "./RegisterBar";
 export default function LoginStatusBar() {
     //state for showing a login form, register form, or logout button.
     const [loginStatus, setLoginStatus] = useState("login");
+    let content = null;
+
+    // swtich(loginStatus) {
+    //     case 'login': content =
+    //         <button onClick={() => { setLoginStatus("register") }}>Register</button>;
+    //     break;
+    // };
+    if (loginStatus === "login") {
+        content = <>
+            <button onClick={() => { setLoginStatus("register") }}>Register</button>
+        </>
+    }
+
+    if (loginStatus === "register") {
+        content = <>
+            <RegisterBar />
+            <button onClick={() => { setLoginStatus("login") }}>Login</button>
+        </>
+    }
+
+    if (loginStatus === "logout") {
+        content = <>
+            logout
+        </>
+    }
 
     return (<>
-        You are: {loginStatus}
-        {loginStatus === "register" && <RegisterBar />}
-        <button onClick={() => { setLoginStatus("login") }}>Login</button>
-        <button onClick={() => { setLoginStatus("register") }}>Register</button>
-        <button onClick={() => { setLoginStatus("logout") }}>Logout</button>
+        {content}
     </>
     )
 }
--- src/components/RegisterBar.jsx
+++ src/components/RegisterBar.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
+function handleRegister() {
+    alert("register");
+}
 export default function RegisterBar() {
     return (
         <>
             <form >
                 <label >name</label>
                 <input type="text" name="name" />
+                <label >email</label>
+                <input type="email" name="email" />
+                <label >password</label>
+                <input type="password" name="password" />
+                <label >password confirmation</label>
+                <input type="password" name="password_confirmation" />
+                <button type="button" name="submit" onClick={handleRegister}>Register</button>
             </form>
         </>
     )
